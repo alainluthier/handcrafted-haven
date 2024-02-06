f45c00f337b89e505a5c4be12b2fcb59c068bccf
alainluthier <alainluthier@byui.edu>
2024-02-06 11:24:47 -0400

    create user working

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -6,6 +6,44 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { signIn } from '../../auth';
 import { AuthError } from 'next-auth';
+import bcrypt from 'bcrypt';
+
+const UserSchema = z.object({
+  id: z.string().optional(),
+  email: z.string(),
+  role: z.enum(['customer', 'artisan']),
+  password: z.string().min(6),
+  confirmPassword: z.string().min(6),
+  name: z.string(),
+  image_url: z.string().optional(),
+  phone: z.string().optional()
+}).superRefine(({ confirmPassword, password }, ctx) => {
+  if (confirmPassword !== password) {
+    ctx.addIssue({
+      code: "custom",
+      message: "The passwords did not match"
+    });
+  }
+});
+
+
+
+export async function createUser(formData: FormData) {
+  const{email,password,role,name} = UserSchema.parse({
+    email: formData.get('email'),
+    password: formData.get('password'),
+    confirmPassword: formData.get('confirm-password'),
+    role: formData.get('type'),
+    name: formData.get('name')
+  })
+  const hashedPassword = await bcrypt.hash(password,10);
+  await sql`
+  INSERT INTO users_ (email, role,password,name)
+  VALUES (${email}, ${role}, ${hashedPassword}, ${name})
+  `;
+
+  redirect('/login');
+}
 
 export async function authenticate(
     prevState: string | undefined,
