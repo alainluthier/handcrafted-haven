6fb7868f5bed11dbdad57e8c67be0dad5ac38554
alainluthier <alainluthier@byui.edu>
2024-02-06 09:44:12 -0400

    middleware and auth

--- src/components/home/sidenav.tsx
+++ src/components/home/sidenav.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import NavLinks from "./nav-links";
 import { PowerIcon } from '@heroicons/react/24/outline';
 import Logo from "../logo";
-import { signOut } from "../../../auth";
+import { signOut } from "../../auth";
 export default function SideNav(){
     return(
         <div className="flex h-full flex-col px-3 py-4 md:px-2">
